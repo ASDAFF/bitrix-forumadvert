b25b83a114776ae6ee1517912105984622a0ffc7
Eldar Rakhimberdin <ibeono@gmail.com>
2015-02-19 23:22:54 +0300

    php 5.4+ compatibility
    Fatal error: Call-time pass-by-reference has been removed

--- src/beono.forumadvert/install/result_modifier.php
+++ src/beono.forumadvert/install/result_modifier.php
@@ -1,5 +1,5 @@
 <?//+beono.forumadvert! DO NOT EDIT THIS BLOCK!
 if(CModule::IncludeModule('beono.forumadvert')) {
-	BeonoForumAdvert::modifyMessageList(&$arResult["MESSAGE_LIST"]);
+	BeonoForumAdvert::modifyMessageList($arResult["MESSAGE_LIST"]);
 }
 //-beono.forumadvert?>
